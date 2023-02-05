fae917dbe86ced47c127e0db04ad052b139a5b6d
PhantomVlad <mmvlad99@mail.ru>
2023-02-05 23:42:27 +0300

    fix sum

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -19,7 +19,7 @@ module Api
       private
 
       def params_require_filer
-        if params.include?(:stats)
+        if params&.require(:stats)[:bonuses] == "sum"
           @flag_sum = true
         end
 
