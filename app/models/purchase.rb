2cbb6d76c37544f43b008d6ff6f1767cff2b7e96
PhantomVlad <mmvlad99@mail.ru>
2023-02-06 20:29:10 +0300

    Add validation

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,17 @@
 class Purchase < ApplicationRecord
   validates :amount, comparison: { greater_than: 0, message: "is required" }
   validates :user_id, presence: { message: "is required" }
+  validates :shop_id, presence: { message: "is required" }
+  validate :user_and_shop
+  def user_and_shop
+    unless Shop.where(id: shop_id).present?
+      errors.add(:shop_id, "not found")
+    end
+
+    unless User.where(id: user_id).present?
+      errors.add(:user_id, "not found")
+    end
+  end
+
+
 end
